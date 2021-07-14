b20c1b3110e465c9def225a60fc4ac7511c1bed4
Daniel Romero <daanmaro99june@gmail.com>
2021-07-14 11:48:39 -0500

    Encapsulando código en funciones

--- figuras.js
+++ figuras.js
@@ -1,55 +1,65 @@
 //Square Code
 console.group('Square')
-const ladoCuadrado = 5
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`)
+// const ladoCuadrado = 5
+// console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`)
 
-const perimetroCuadrado = ladoCuadrado * 4
-console.log(`El perímetro del cuadrado es ${perimetroCuadrado}cm`)
+function perimetroCuadrado(lado) {
+    return lado * 4
+} 
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado
-console.log(`El área del cuadrado es: ${areaCuadrado}cm^2`)
+// console.log(`El perímetro del cuadrado es ${perimetroCuadrado}cm`)
+
+// const areaCuadrado = ladoCuadrado * ladoCuadrado
+function areaCuadrado(lado) {
+    return lado * lado
+}
+// console.log(`El área del cuadrado es: ${areaCuadrado}cm^2`)
 console.groupEnd()
 
 //Triangle Code
 console.group('Triangle')
-const sideTriangle1 = 6
-const sideTriangle2 = 6
-const baseTriangle = 4
-const heightTriangle = 5.5
+// const sideTriangle1 = 6
+// const sideTriangle2 = 6
+// const baseTriangle = 4
+// const heightTriangle = 5.5
 
-console.log(
-    `Los lados del triángulo miden: 
-    ${sideTriangle1}cm, 
-    ${sideTriangle2}cm, 
-    ${baseTriangle}cm`)
-console.log(`El altura del triángulo es: ${heightTriangle}cm`)
+// console.log(
+//     `Los lados del triángulo miden: 
+//     ${sideTriangle1}cm, 
+//     ${sideTriangle2}cm, 
+//     ${baseTriangle}cm`)
+// console.log(`El altura del triángulo es: ${heightTriangle}cm`)
 
-const perimetroTrinagle = sideTriangle1 + sideTriangle2 + baseTriangle
-console.log(`El perímetro del tríangulo es: ${perimetroTrinagle}cm`)
+function perimetroTrinagle (side1, side2, base) {
+    return side1 + side2 + base
+}
 
-const areaTriangle = (baseTriangle * heightTriangle) / 2
-console.log(`El área del triángulo es: ${areaTriangle}cm^2`)
+
+function areaTriangle(base, height) {
+    return (base * height) / 2
+}
 
 console.groupEnd()
 
 //Circle Code
 console.group('Circle')
 //Radio
-const radioCircle = 4
-console.log(`El radio del círculo es: ${radioCircle}cm`)
+
 //Diameter
-const diameterCircle = radioCircle * 2
-console.log(`El diámetro es: ${diameterCircle}cm`)
+function diameterCircle(radio){
+   return radio * 2 
+}
 //PI
 const PI = Math.PI
 console.log(`PI: ${PI}`)
 //Circumference
-const  perimetroCircle = diameterCircle * PI
-console.log(`El perímetro de la circunferencia es: ${perimetroCircle}cm`)
+function perimetroCircle (radio) {
+    const diameter = diameterCircle(radio)
+    return diameter * PI
+}
 //Area
-const areaCircle = (radioCircle * radioCircle) * PI
-console.log(`El área del círculo es: ${areaCircle}`)
-
-
+function areaCircle (radio) {
+    return (radio * radio) * PI
+}
 
 console.groupEnd()
