d35b13719afb12dea14f1a1389a76a9a2cdc01a8
Daniel Romero <daanmaro99june@gmail.com>
2021-08-09 13:12:45 -0500

    Discounts

--- descuentos.js
+++ descuentos.js
@@ -9,6 +9,17 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento
 }
 
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById('InputPrice')
+    const priceValue = inputPrice.value
+    const inputDiscount = document.getElementById('InputDiscount')
+    const priceDiscount = inputDiscount.value
+
+    const preicioConDescuento = calcularPrecioConDescuento(priceValue, priceDiscount)
+
+    const resultP = document.getElementById("ResultPrice")
+    resultP.innerText = `El precio con descuento son $${preicioConDescuento}`
+}
 
 
 // console.log({
