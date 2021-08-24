88f20de5d3384e9e4e942c304739b07e34b40ca9
Daniel Romero <daanmaro99june@gmail.com>
2021-08-24 15:17:09 -0500

    Discounts plus coupons

--- 2Taller/descuentos.js
+++ 2Taller/descuentos.js
@@ -1,6 +1,21 @@
 // const precioOriginal = 120
 // const descuento = 18
 
+function calculardescuentos(FirstDescuento, SegundoDescuento) {
+    const descuentos = []
+    descuentos.push(FirstDescuento)
+    descuentos.push(SegundoDescuento)
+
+    const sumaDescuentos = descuentos.reduce((a, b) => a + b)
+    const productoDescuentos = descuentos.reduce((a, b) => a * b)
+    //Function reduce; a take the first value, and b the second,
+    //but if there another value a take the value of a + b and b take the third value
+    // and so on, so on.
+    const descuentoTotal = (sumaDescuentos) - (productoDescuentos/100)
+    
+    return descuentoTotal
+
+}
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento
@@ -11,11 +26,17 @@ function calcularPrecioConDescuento(precio, descuento) {
 
 function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById('InputPrice')
-    const priceValue = inputPrice.value
-    const inputDiscount = document.getElementById('InputDiscount')
-    const priceDiscount = inputDiscount.value
+    const priceValue = parseInt(inputPrice.value)
+    
+    const inputFirstDiscount = document.getElementById('FirstDiscount')
+    const priceFirstDiscount = parseInt(inputFirstDiscount.value)
+    const inputSecondDiscount = document.getElementById('SecondDiscount')
+    const priceSecondDiscount = parseInt(inputSecondDiscount.value)
 
-    const preicioConDescuento = calcularPrecioConDescuento(priceValue, priceDiscount)
+    const descuentoTotal = calculardescuentos(priceFirstDiscount, priceSecondDiscount)
+    
+
+    const preicioConDescuento = calcularPrecioConDescuento(priceValue, descuentoTotal)
 
     const resultP = document.getElementById("ResultPrice")
     resultP.innerText = `El precio con descuento son $${preicioConDescuento}`
@@ -28,3 +49,4 @@ function onClickButtonPriceDiscount() {
 //     porcentajePrecioConDescuento,
 //     precioConDescuento
 // })
+
